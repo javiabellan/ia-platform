147d8bf60123973398819f2442a461b829045c07
Javi <franfdk17@gmail.com>
2019-11-04 18:27:27 +0100

    ordered cat variables

--- BACKEND/backend.py
+++ BACKEND/backend.py
@@ -171,19 +171,22 @@ class Dataset(Resource):
 		"""
 		Get 1 dataset by Id (Data & metadata)
 		"""
-
-		# Get dataset metadata (From quering database)
+		################ Get dataset metadata (From quering database)
 		dataset_metadata = DatasetTable.query.get(id).to_dict()
 
-		# Get dataset data (From reading CSV)
+
+		################ Get dataset data (From reading CSV)
 		global DATAFRAME
 		filePath     = os.path.join(FILES_DIR, dataset_metadata["file"])
 		DATAFRAME    = pandas.read_csv(filePath)
-		dataset_head = DATAFRAME.head(15).to_dict(orient='split')
+		#if "head"
+		dataset_data = DATAFRAME.head(15).to_dict(orient='split')
+		#if "wholeDataset":
+		#dataset_data = DATAFRAME.to_dict(orient='records')
 
 		return {
 			"metadata": dataset_metadata,
-			"data":     dataset_head
+			"data":     dataset_data
 		}
 
 
@@ -205,6 +208,24 @@ class Dataset(Resource):
 		return dataset2delete.to_dict()
 
 
+@apiDataset.route('/whole/<string:id>')
+class WholeDataset(Resource):
+
+	def get(self, id): ################################ GET DATASET (DATA & METADATA)
+		"""
+		Get 1 dataset by Id (Data & metadata)
+		"""
+		################ Get dataset metadata (From quering database)
+		dataset_metadata = DatasetTable.query.get(id).to_dict()
+
+
+		################ Get dataset data (From reading CSV)
+		global DATAFRAME
+		filePath     = os.path.join(FILES_DIR, dataset_metadata["file"])
+		DATAFRAME    = pandas.read_csv(filePath)
+		dataset_data = DATAFRAME.to_dict(orient='records')
+
+		return dataset_data
 
 ################################################################ DOWNLOAD FILE
 # @app.route("/download/<file>")
@@ -227,9 +248,94 @@ class Dataset(Resource):
 ################################################################
 ################################################################  ENDPOINTS
 ################################################################  VISUALIZATION
+################################################################
 
-@apiVisual.route('/vega')
-class ChartHello(Resource):
+
+################################### AUXILIAR PANDAS FUNCS
+def get_varNames(df):
+	return df.columns.values.tolist()
+
+def get_varTypes(df):
+	types = [dtype.name for dtype in df.dtypes.values.tolist()]
+	types = [t.replace('object',         'cat')  for t in types]
+	types = [t.replace('category',       'cat')  for t in types]
+	types = [t.replace('bool',           'cat')  for t in types]
+	types = [t.replace('int64',          'num')  for t in types]
+	types = [t.replace('float64',        'num')  for t in types]
+	types = [t.replace('datetime64[ns]', 'date') for t in types]
+	types = [t.replace('timedelta[ns]',  'date') for t in types]
+	return types
+
+
+################################### AUXILIAR ALTAIR FUNCS
+def chart_base(varName):
+	return alt.Chart().mark_bar().properties(
+		width=300, height=200,
+		title=varName
+	)
+
+def chart_numerical(varName):
+	return chart_base(varName).encode(
+		x=alt.X(varName, type='quantitative', bin=alt.Bin(maxbins=16), title=None),
+		y=alt.Y('count()', axis=None)
+	)
+
+def chart_categorical(varName):
+	return chart_base(varName).encode(
+		x=alt.X('count()', axis=None),
+		y=alt.Y(varName, type='nominal', title=None,
+			sort=alt.EncodingSortField(field=varName, op="count", order="descending")),
+	)
+
+
+
+@apiVisual.route('/univariate')
+class Univariate(Resource):
+
+	def univariate_charts(df, data_url, include_num=True, include_cat=True, include_date=False):
+	    allVarNames = get_varNames(df)
+	    allVarTypes = get_varTypes(df)
+	    
+	    varNames = []
+	    varTypes = []
+	    charts   = []
+	    filters  = []
+	    
+	    # 1) Generate charts and filters
+	    for varName, varType in zip(allVarNames, allVarTypes):
+	        if include_num and varType=="num":
+	            varNames.append(varName)
+	            varTypes.append(varType)
+	            charts.append(chart_numerical(varName))
+	            filters.append(alt.selection_interval(name=varName, encodings=['x']))
+	        elif include_cat and varType=="cat":
+	            varNames.append(varName)
+	            varTypes.append(varType)
+	            charts.append(chart_categorical(varName))
+	            filters.append(alt.selection_multi(name=varName, encodings=['y']))
+	    
+	    # 2) Crossfilter charts
+	    filteredCharts = []
+	    for (chart, myFilter, varName, varType) in zip(charts, filters, varNames, varTypes):
+	        # Colors
+	        if varType=="num":
+	            background = chart.mark_bar(color='teal', opacity=0.15)
+	            highlight  = chart.mark_bar(color='teal')
+
+	        if varType=="cat":
+	            background = chart.mark_bar(color='lightgray')
+	            highlight  = chart.encode(color=varName + ":N")
+
+	        # Add my filter to modify others
+	        background = background.add_selection(myFilter)
+
+	        # Add others filters that modify me
+	        for f in filters: highlight = highlight.transform_filter(f)
+
+	        filteredCharts.append(alt.layer(background, highlight))
+	    
+	    return alt.concat(*filteredCharts, data=data_url, columns=2)
+
 
 	def get(self): ################################ GET CHART
 
